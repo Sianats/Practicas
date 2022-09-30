741a065e1c9711165e98102019d04f5ae4a9a931
Sianats <79252137+Sianats@users.noreply.github.com>
2022-09-30 17:39:33 +0200

    Año bisiesto acabado

--- Añobisiesto.py
+++ Añobisiesto.py
@@ -1,12 +1,27 @@
+# Creo una función con una variable n que será el año a comprobar.
 
-def esBisiesto (n):
+def esBisiesto(n):
 
-    
+  # Declaro la variable sol como booleano para que me devuelva una respuesta de True o False.
 
+  sol = bool
 
-print(f"400 Sí es bisiesto 400%4: {400%4}")
-print(f"1000 No es bisiesto 1000%100: {1000%100}")
-print(f"1000 Sí es bisiesto 4000%400: {4000%400}")
+  # Ajusto un bucle que tiene en cuenta la expeción: si un año es multiplo de 100,
+  # no es bisiesto a no ser que tambien sea múltiplo de 400.
+
+  if n%400==0:
+    sol = True
+  else:
+    if n%100==0:
+      sol = False
+    else:
+      if n%4==0:
+        sol = True
+      else:
+        sol = False
+  return sol
+
+# Pruebo los ejemplos dados en clase con mi función pra que compruebe si son o no bisiestos:
 
 print(esBisiesto(2022)) #False
 print(esBisiesto(2021)) #False
